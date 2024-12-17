1dd202bb5e1e3b76553c688cdac41cd3cf3f1abd
Donald Green <donaldgreen2024@gmail.com>
2024-12-17 13:49:51 -0800

    push

---
+++ ShapeMatcher3.py
@@ -0,0 +1,40 @@
+import pygame
+
+pygame.init()
+screenWidth = 1280
+screenHeight = 720
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Shape Matcher 3")
+clock = pygame.time.Clock()
+running = True
+
+x = 50
+y = 50
+width = 150
+height = 150
+vel = 25
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             running = False
+    
+    pygame.draw.rect(screen, (255,0,0), (x, y, width, height))
+    pygame.display.update()
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a] and x > vel - vel:
+        x -= vel
+    if keys[pygame.K_d] and x < screenWidth - width:
+        x += vel
+    if keys[pygame.K_w] and y > vel - vel:
+        y -= vel
+    if keys[pygame.K_s] and y < screenHeight - height:
+        y += vel
+        
+    screen.fill((131,137,150))
+    
+    clock.tick(60)
+
+pygame.quit()
